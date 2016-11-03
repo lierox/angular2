e9801451b4953f1966e2d6b4fa38459257583f28
lierox <furkankostekci@hotmail.com>
2016-11-03 17:18:23 +0300

    topics folder files removed

--- client/imports/app/topics/index.ts
+++
@@ -1,7 +0,0 @@
-import { TopicsListComponent } from './topics-list.component';
-import { TopicDetailsComponent } from './topic-details.component';
-
-export const TOPICS_DECLARATIONS = [
-  TopicsListComponent,
-  TopicDetailsComponent
-];
